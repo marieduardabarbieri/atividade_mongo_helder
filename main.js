aeff0d59f4355e4385bb4d8a31c1690bb6bccf98
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-05-12 10:42:03 -0400

    Chamando os  comandos CRUD para testá-los na main
    Comando para rodar no terminal: node main.js

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ async function main() {
     await conectar();
 
     const livroRepository = new Livro();
+
+    /* COMANDO PARA CADASTRAR
+     await livroRepository.cadastrarLivro({
+      titulo: "Dom Casmurro",
+      autor: "Machado de Assis",
+      ano: 1899,
+      disponivel: true,
+    });*/
+
+    /* COMANDO PARA LISTAR
+    await livroRepository.listarLivros();*/
+
+    /*COMANDO PARA BUSCAR POR TITULO
+    wait livroRepository.buscarLivroPorTitulo("A Troca");*/
+
+    /* COMANDO PARA ATUALIZAR
+    await livroRepository.atualizarLivro("6a033382b96d56d18fc31f34", {
+      titulo: "A Troca",
+    });*/
+
+    /* COMANDO PARA REMOVER
+    await livroRepository.removerLivro("6a033382b96d56d18fc31f34");*/
   } catch (error) {
     console.log("Erro na aplicação:", error.message);
   }
